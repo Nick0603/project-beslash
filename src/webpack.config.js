ae191ff01d88d3ad691955dbe73955775a52b1bf
LIOU <johnny.liou1990@gmail.com>
2017-10-09 18:30:10 +0800

    add webpack async plugin

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,61 +1,68 @@
+import webpack from 'webpack'
 import path from 'path'
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-var webpack = require('webpack');
-var colors = require('colors');
-// var extractCSS = new ExtractTextPlugin('css/[name].css');
+import colors from 'colors'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 const extractSass = new ExtractTextPlugin({
-    filename: "css/[name].css"
-    , disable: process.env.NODE_ENV === "development"
-});
+    filename: "css/[name].css",
+    disable: process.env.NODE_ENV === "development"
+})
 
-var webpack_configs = webpack({
-    entry: {
-        // index: './../public/bundle/index'
-        index: path.join(__dirname, './../public/bundle/index')
-    },
-    output: {
-        // path: __dirname + '/public/webpackBale',
-        path: path.join(__dirname, '/../public/webpackBale'),
-        filename: 'js/[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader!resolve-url-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "sass-loader"
-                    }],
-                    // use style-loader in development
-                    fallback: "style-loader"
-                })
-            },
-            {
-                test: /\.jsx$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['stage-3', 'react']
-                }
-            },
-            {
-                test: /\.png|jpg$/,
-                loader: 'url-loader'
-            }
-        ]
-    },
-    plugins: [
-        extractSass
-        // , new webpack.optimize.UglifyJsPlugin({minimize: true})
-    ]
-});
+const webpack_configs = webpack({
+  entry: {
+    index: path.join(__dirname, '../public/bundle/index'),
+  },
+  output: {
+      path: path.join(__dirname, '../public/webpackBale'),
+      filename: 'js/[name].js'
+  },
+  module: {
+      rules: [
+          {
+              test: /\.css$/,
+              loader: "style-loader!css-loader!resolve-url-loader"
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.scss$/,
+            use: extractSass.extract({
+              use: [{
+                loader: 'css-loader',
+              }, {
+                loader: 'sass-loader',
+              }],
+                // use style-loader in development
+              fallback: 'style-loader',
+            }),
+          },
+          {
+              test: /\.jsx$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+              options: {
+                  plugins: ['transform-runtime'],
+                  presets: ['es2015', 'react']
+              }
+          },
+          {
+              test: /\.png|jpg$/,
+              loader: 'url-loader'
+          }
+      ]
+  },
+  plugins: [
+    extractSass,
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+  ]
+})
 
 webpack_configs.watch({ // watch options:
     aggregateTimeout: 400, // wait so long for more changes
@@ -63,8 +70,8 @@ webpack_configs.watch({ // watch options:
     // pass a number to set the polling interval
 }, function(err, stats) {
     if(err){
-        console.warn(colors.red(err));
+        console.warn(colors.red(err))
     }
-});
+})
 
-module.exports = webpack_configs;
+export default webpack_configs
